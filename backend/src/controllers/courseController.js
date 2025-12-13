ae163bbdcb09decaff17cb7ec9695fbcdba07a61
CenturyFinanceLtd <dvisro13@gmail.com>
2025-12-13 15:18:36 +0530

    one

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -1321,7 +1321,7 @@ const validateRawCourse = (input) => {
     'name','programme','programmeSlug','courseSlug','slug',
     'hero','stats','aboutProgram','learn','skills','details','capstone',
     'careerOutcomes','toolsFrameworks','modules','instructors','offeredBy',
-    'image','media'
+    'image','media','targetAudience','prereqsList'
   ]);
 
   const unexpected = Object.keys(input || {}).filter((k) => !allowRoot.has(k));
@@ -1452,6 +1452,8 @@ const validateRawCourse = (input) => {
   out.skills = strArr(input.skills || [], 'skills');
   out.careerOutcomes = strArr(input.careerOutcomes || [], 'careerOutcomes');
   out.toolsFrameworks = strArr(input.toolsFrameworks || [], 'toolsFrameworks');
+  out.targetAudience = strArr(input.targetAudience || [], 'targetAudience');
+  out.prereqsList = strArr(input.prereqsList || [], 'prereqsList');
 
   // details
   const detailsAllowed = new Set(['effort','language','prerequisites']);
@@ -1590,7 +1592,8 @@ const getRawCourseBySlug = asyncHandler(async (req, res) => {
   const allowRoot = new Set([
     'name','programme','programmeSlug','courseSlug','slug',
     'hero','stats','aboutProgram','learn','skills','details','capstone',
-    'careerOutcomes','toolsFrameworks','modules','instructors','offeredBy','image','media'
+    'careerOutcomes','toolsFrameworks','modules','instructors','offeredBy','image','media',
+    'targetAudience','prereqsList'
   ]);
   const sanitized = {};
   for (const key of allowRoot) {
