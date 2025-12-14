5378d57b53c5c9f16bf7899b1acdfa82690a6bb4
Nishant Singh <128042006+nishantsingh505@users.noreply.github.com>
2025-12-14 19:12:42 +0530

    done

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -13,21 +13,21 @@ export default defineConfig({
     },
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // IMPORTANT: only split pure, non-React libs.
-            if (id.includes('node_modules/bootstrap')) return 'bootstrap'
-            if (id.includes('node_modules/apexcharts/')) return 'charts'
-            if (id.includes('node_modules/lightgallery/')) return 'lightgallery'
-            if (id.includes('node_modules/datatables.')) return 'datatables'
-            if (id.includes('node_modules/@fullcalendar/')) return 'fullcalendar'
-            if (id.includes('node_modules/jsvectormap/')) return 'jsvectormap'
-            if (id.includes('node_modules/slick-carousel/')) return 'slick'
-            if (id.includes('node_modules/magnific-popup/')) return 'magnific'
-            if (id.includes('node_modules/prism')) return 'prism'
-            return 'vendor'
-          }
-        },
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     // IMPORTANT: only split pure, non-React libs.
+        //     if (id.includes('node_modules/bootstrap')) return 'bootstrap'
+        //     if (id.includes('node_modules/apexcharts/')) return 'charts'
+        //     if (id.includes('node_modules/lightgallery/')) return 'lightgallery'
+        //     if (id.includes('node_modules/datatables.')) return 'datatables'
+        //     if (id.includes('node_modules/@fullcalendar/')) return 'fullcalendar'
+        //     if (id.includes('node_modules/jsvectormap/')) return 'jsvectormap'
+        //     if (id.includes('node_modules/slick-carousel/')) return 'slick'
+        //     if (id.includes('node_modules/magnific-popup/')) return 'magnific'
+        //     if (id.includes('node_modules/prism')) return 'prism'
+        //     return 'vendor'
+        //   }
+        // },
       },
     },
   },
